2cb09ea6f97432161c729a3855c8f69ffa1f54cf
Pranjal0512 <pranjal.barnwal051234@gmail.com>
2023-11-09 17:26:51 +0545

    models compeleted except project

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Service)
+admin.site.register(Testimonial)
+admin.site.register(Skill)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Information)
+
+
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
+    subject = models.TextField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=300)
+    logo = models.CharField(max_length=300)
+    description = models.TextField()
+    # money = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Testimonial(models.Model):
+    image = models.TextField()
+    name = models.CharField(max_length=300)
+    Post = models.CharField(max_length=300)
+    Description = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    name=models.CharField(max_length=300)
+    level=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    name=models.CharField(max_length=300)
+    starting_year=models.IntegerField()
+    ending_year=models.IntegerField()
+    university=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    name=models.CharField(max_length=300)
+    starting_year=models.IntegerField()
+    ending_year=models.IntegerField()
+    university=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Information(models.Model):
+    address= models.CharField(max_length=400)
+    phone= models.IntegerField()
+    email= models.EmailField(max_length=15)
+    def __str__(self):
+        return self.address
+
+
+
+
